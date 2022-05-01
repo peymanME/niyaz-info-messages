4ff2bd36badbdb43685eeca8340cf735c2bfd9a4
peymanME <peymanmostafaie@gmail.com>
2022-05-01 18:42:13 +0200

    Modify MessageComponentTest class

---
+++ src/test/java/com/niyaz/info/messages/component/MessageComponentTest.java
@@ -0,0 +1,101 @@
+package com.niyaz.info.messages.component;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+
+/**
+ * Created by:
+ *
+ * @author Peyman Mostafai Ekhtiar
+ * @project niyaz-info-messages
+ * @date 2022-05-01
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader= AnnotationConfigContextLoader.class)
+public class MessageComponentTest {
+
+    @Configuration
+    public static class ContextConfiguration {
+        @Bean
+        public ReloadableResourceBundleMessageSource messageSource() {
+            ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+            source.setBasenames("classpath:messages");
+            source.setDefaultEncoding("UTF-8");
+            return source;
+        }
+        @Bean
+        public LocalValidatorFactoryBean getValidator() {
+            LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+            bean.setValidationMessageSource(messageSource());
+            return bean;
+        }
+        @Bean
+        public MessageComponent messageComponent(MessageSource messageSource){
+            return new MessageComponent(messageSource);
+        }
+
+    }
+
+    @Autowired
+    private MessageComponent messageComponent;
+
+    private enum LOCALIZATION{
+        FA ("fa", "IR"),
+        US ("en", "US"),
+        PL ("pl", "PL");
+
+        private String language;
+        private String region;
+
+        LOCALIZATION (String _language, String _region){
+            language = _language;
+            region = _region;
+        }
+
+
+        public String getLanguage(){
+            return language;
+        }
+
+        public String getRegion(){
+            return region;
+        }
+
+
+    }
+
+    private Locale getLocale(LOCALIZATION localization){
+        return new Locale(localization.language, localization.region);
+    }
+
+
+    @Test
+    public void check_simple_message_EN() {
+        String message = messageComponent.get("application.test.simple", getLocale(LOCALIZATION.US));
+        Assert.assertEquals("This is simple", message);
+    }
+    @Test
+    public void check_simple_message_IR() {
+        String message = messageComponent.get("application.test.simple",getLocale(LOCALIZATION.FA));
+        Assert.assertEquals("این ساده است", message);
+    }
+    @Test
+    public void check_simple_message_PL() {
+        String message = messageComponent.get("application.test.simple", getLocale(LOCALIZATION.PL));
+        Assert.assertEquals("To jest proste", message);
+    }
+
+}
